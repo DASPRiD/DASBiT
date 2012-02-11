47027153b623d102656182d9aefe18bab9479268
Ben Scholzen <mail@dasprids.de>
2012-02-11 20:29:45 +0100

    Finished CTCP parser

--- dasbit/irc/ctcp.py
+++ dasbit/irc/ctcp.py
@@ -11,7 +11,7 @@ class Ctcp:
             '\0':        '0',
             '\r':        'r',
             '\n':        'n'
-        }        
+        }
 
         self.xQuoteMap = {
             self.xQuote:    self.xQuote,
@@ -50,7 +50,7 @@ class Ctcp:
         if tag is not None:
             if not tag in self.allowedTags:
                 raise Exception('%s is not a valid tag' % tag)
-            
+
             message += self.ctcpQuote(tag)
 
             if data is not None:
@@ -60,6 +60,61 @@ class Ctcp:
 
         return message
 
+    def unpackMessage(self, message):
+        message         = self.lowLevelDequote(message)
+        parts           = []
+        length          = len(message)
+        currentPos      = 0
+        part            = ''
+        isExtended      = None
+        standardMessage = ''
+
+        while currentPos < length:
+            if isExtended is None:
+                if message[currentPos] == self.delimiter:
+                    isExtended = True
+                else:
+                    isExtended = False
+
+                part = ''
+            else:
+                if message[currentPos] == self.delimiter:
+                    if isExtended:
+                        extendedMessage = self.parseExtendedMessage(part)
+
+                        if extendedMessage is not None:
+                            parts.append(extendedMessage)
+
+                        isExtended = False
+                    else:
+                        standardMessage += part
+                        isExtended = True
+                        part       = ''
+                else:
+                    part += message[currentPos]
+
+            currentPos += 1
+
+        if part:
+            standardMessage += part
+
+        if standardMessage:
+            parts.append(standardMessage)
+
+        return parts
+
+    def parseExtendedMessage(self, message):
+        if not message:
+            return None
+
+        if ' ' in message:
+            tag, data = message.split(' ', 1)
+        else:
+            tag  = message
+            data = None
+
+        return (tag, data)
+
     def ctcpQuote(self, string):
         return self.quote(self.xQuoteMap, self.xQuote, self.xQuote + self.delimiter, string)
 
@@ -67,10 +122,10 @@ class Ctcp:
         return self.dequote(self.xQuoteMap, self.xQuote, string)
 
     def lowLevelQuote(self, string):
-        return self.quote(self.mQuoteMap, self.mQuote, self.mQuote + '\0\n\r', string)    
+        return self.quote(self.mQuoteMap, self.mQuote, self.mQuote + '\0\n\r', string)
 
     def lowLevelDequote(self, string):
-        return self.dequote(self.mQuoteMap, self.mQuote, string)    
+        return self.dequote(self.mQuoteMap, self.mQuote, string)
 
     def quote(self, quoteMap, quoteChar, specialChars, string):
         for specialChar in specialChars:
@@ -99,92 +154,3 @@ class Ctcp:
                 currentPos += 1
 
         return result
-"""
-
-    /**
-     * Unpack a message.
-     *
-     * The returned array will contain all splitted standard and extended
-     * messages. Standard messages will simply be strings, while extended
-     * messages will be represented as arrays containing 'tag' and 'data'.
-     *
-     * @param  string $message
-     * @return array
-     */
-    public function unpackMessage($message)
-    {
-        $message = $this->lowLevelDequote($message);
-
-        preg_match_all(
-            "("
-            . "(" . self::DELIMITER . ")?"
-            . "(?(1)"
-            .   "[^" . self::DELIMITER . "]*" . self::DELIMITER
-            . "|"
-            .   ".+?(?:$|(?=" . self::DELIMITER ."))"
-            . "))Ss",
-            $message,
-            $matches
-        );
-
-        $parts           = array();
-        $standardMessage = '';
-
-        foreach ($matches[0] as $match) {
-            $match = $this->ctcpDequote($match);
-
-            if ($match[0] === self::DELIMITER) {
-                $extendedMessage = $this->parseExtendedMessage($match);
-
-                if ($extendedMessage !== null) {
-                    $parts[] = $extendedMessage;
-                }
-            } else {
-                $standardMessage .= $match;
-            }
-        }
-
-        if (!empty($standardMessage)) {
-            $parts[] = $standardMessage;
-        }
-
-        return $parts;
-    }
-
-    /**
-     * Parse an extended message.
-     *
-     * @param  string $message
-     * @return array
-     */
-    protected function parseExtendedMessage($message)
-    {
-        $result = preg_match(
-            "(" . self::DELIMITER
-            . "(?:(?<tag>[^\1\40]+)(?: (?<data>[^\1]*))?)?"
-            . self::DELIMITER . ")",
-            $message,
-            $matches
-        );
-
-        if ($result === 1 && isset($matches['tag'])) {
-            if (!in_array($matches['tag'], $this->allowedTags)) {
-                return null;
-            }
-
-            $result = array(
-                'tag' => $matches['tag']
-            );
-
-            if (isset($matches['data'])) {
-                $result['data'] = $matches['data'];
-            } else {
-                $result['data'] = null;
-            }
-
-            return $result;
-        }
-
-        return null;
-    }
-"""
