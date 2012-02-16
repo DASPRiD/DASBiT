5f3e3f0cac4bd2cc86a64b89cdefb88d61e76993
Ben Scholzen <mail@dasprids.de>
2012-02-16 17:23:11 +0100

    Added remind plugin

---
+++ dasbit/plugin/remind.py
@@ -0,0 +1,37 @@
+import os
+from dasbit.core import Config
+
+class Remind:
+    def __init__(self, manager):
+        self.client = manager.client
+        self.config = Config(os.path.join(manager.dataPath, 'remind'))
+
+        manager.registerCommand('remind', 'remind', 'remind', '(?P<nickname>[^ ]+) to (?P<message>.+)', self.remind)
+        manager.registerMessage('remind', self.checkReminder)
+
+    def remind(self, source, nickname, message):
+        if not nickname in self.config:
+            self.config[nickname] = []
+
+        self.config[nickname].append({
+            'message': message,
+            'from':    source.prefix['nickname']
+        })
+
+        self.client.reply(source, 'Reminder stored')
+
+    def checkReminder(self, message):
+        if not message.prefix['nickname'] in self.config:
+            return
+
+        nickname = message.prefix['nickname']
+
+        for reminder in self.config[nickname]:
+            self.client.reply(
+                message,
+                '%s, %s wants me to remind you to %s' % (
+                    nickname, reminder['from'], reminder['message']
+                )
+            )
+
+        del self.config[nickname]
