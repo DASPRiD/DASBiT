3172e750d6129197ee11b8e62c10d77e59aa3f8c
Ben Scholzen <mail@dasprids.de>
2012-06-04 21:07:12 +0200

    Add time-set command

--- dasbit/plugin/now.py
+++ dasbit/plugin/now.py
@@ -1,17 +1,37 @@
+import os
 import pytz
 import datetime
 from time import time
+from dasbit.core import Config
 
 class Now:
     def __init__(self, manager):
         self.client = manager.client
+        self.config = Config(os.path.join(manager.dataPath, 'now'))
 
-        manager.registerCommand('now', 'time', 'time', '(?P<timezone>[^ ]+)?', self.getTime)
+        manager.registerCommand('now', 'time', 'time', '(?P<identifier>[^ ]+)?', self.getTime)
+        manager.registerCommand('now', 'time-set', 'time-set', '(?P<timezone>[^ ]+)', self.setTime)
 
-    def getTime(self, source, timezone):
+    def setTime(self, source, timezone):
+        try:
+            tz = pytz.timezone(timezone)
+        except pytz.UnknownTimeZoneError:
+            self.client.reply(source, '%s is not a valid timezone' % timezone, 'notice')
+            return
+
+        self.config[source.prefix['nickname']] = timezone
+
+        self.client.reply(source, 'Your timezone has been set', 'notice')
+
+    def getTime(self, source, identifier):
         now = datetime.datetime.fromtimestamp(time(), pytz.utc)
 
-        if timezone:
+        if identifier:
+            if identifier in self.config:
+                timezone = self.config[identifier]
+            else:
+                timezone = identifier
+
             try:
                 tz = pytz.timezone(timezone)
             except pytz.UnknownTimeZoneError:
