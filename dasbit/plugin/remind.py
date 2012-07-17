903b9f2005ae8994b110f5b323fd230e48dd5a56
Ben Scholzen <mail@dasprids.de>
2012-07-17 13:42:26 +0200

    Add relative time to reminders

--- dasbit/plugin/remind.py
+++ dasbit/plugin/remind.py
@@ -1,5 +1,7 @@
 import os
 from dasbit.core import Config
+from time import time
+import datetime
 
 class Remind:
     def __init__(self, manager):
@@ -15,7 +17,8 @@ class Remind:
 
         self.config[nickname].append({
             'message': message,
-            'from':    source.prefix['nickname']
+            'from':    source.prefix['nickname'],
+            'time':    time()
         })
         self.config.save()
 
@@ -28,12 +31,54 @@ class Remind:
         nickname = message.prefix['nickname']
 
         for reminder in self.config[nickname]:
+            # Fallback for old reminders
+            if not 'time' in reminder:
+                date = datetime.datetime.utcnow()
+            else:
+                date = datetime.datetime.utcfromtimestamp(reminder['time'])
+
             self.client.reply(
                 message,
-                '%s, %s wants me to remind you %s' % (
-                    nickname, reminder['from'], reminder['message']
+                '%s, %s wants me to remind you %s (written %s)' % (
+                    nickname, reminder['from'], reminder['message'], self._timesince(date)
                 )
             )
 
         del self.config[nickname]
         self.config.save()
+
+    def _timesince(self, date):
+        chunks = (
+            (60 * 60 * 24 * 365, lambda n: 'year' if n is 1 else 'years'),
+            (60 * 60 * 24 * 30, lambda n: 'month' if n is 1 else 'months'),
+            (60 * 60 * 24 * 7, lambda n: 'week' if n is 1 else 'weeks'),
+            (60 * 60 * 24, lambda n: 'day' if n is 1 else 'days'),
+            (60 * 60, lambda n: 'hour' if n is 1 else 'hours'),
+            (60, lambda n: 'minute' if n is 1 else 'minutes')
+        )
+
+        now   = datetime.datetime.utcnow()
+        delta = now - date
+        since = delta.days * 24 * 60 * 60 + delta.seconds
+
+        if since <= 0:
+            return '0 minutes ago'
+
+        for i, (seconds, name) in enumerate(chunks):
+            count = since // seconds
+
+            if count != 0:
+                break
+
+        s = '%d %s' % (count, name(count))
+
+        if i + 1 < len(chunks):
+            seconds2, name2 = chunks[i + 1]
+            count2 = (since - (seconds * count)) // seconds2
+
+            if count2 != 0:
+                s += ' and %d %s' % (count2, name2(count2))
+
+        s += ' ago'
+
+        return s
